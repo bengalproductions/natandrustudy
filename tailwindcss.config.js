c54fd4d52b63e11e3cbd34d45b6a946d0a2b096e
Eddie Coldrick <eddiecoldrick@hotmail.co.uk>
2022-01-11 15:41:39 +0000

    Create tailwindcss.config.js

---
+++ tailwindcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'bengal-orange': '#e67323ff',
+        'bengal-orange-highlight':'#f57d1eff'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
