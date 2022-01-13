2756654de2be0cc4cd034b95efd73583bd90ad98
Eddie Coldrick <eddiecoldrick@hotmail.co.uk>
2022-01-13 20:58:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,36 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/history", (req, res) => {
-  res.sendFile(__dirname + "/views/history.html");
+function search(nameKey, myArray){
+  for (var i=0; i < myArray.length; i++) {
+      if (myArray[i].topic === nameKey) {
+          return myArray[i];
+      }
+  }
+}
+
+app.get("/history", async (req, res) => {
+  const {value: doc} = await db.fetch({
+    "subject": "history"
+}).next();
+var topics = []
+doc.forEach((element)=>{
+if(search(element.topic, topics)){
+  let obj = topics.find(x => x.topic === element.topic);
+  let index = topics.indexOf(obj);
+  topics[index].notes.push(element)
+}else{
+  topics.push({topic:element.topic, notes: [element]})
+}
+})
+topics.forEach((element2)=>{
+  element2.notes.sort((a, b) => (a.numberintopic > b.numberintopic) ? 1 : -1)
+})
+    res.render(__dirname + "/views/history.ejs", {topics: topics})
 });
 
-app.get("/science", (req, res) => {
-  res.sendFile(__dirname + "/views/science.html");
+app.get("/business", (req, res) => {
+  res.sendFile(__dirname + "/views/business.html");
 });
 
 app.get("/geography", (req, res) => {
@@ -32,8 +56,52 @@ app.get("/religion", (req, res) => {
   res.sendFile(__dirname + "/views/religion.html");
 });
 
+app.get("/admin/makenotes", (req,res)=>{
+  res.sendFile(__dirname + "/views/makenotespage.html");
+})
+
+app.get("/notes/:slug", async (req,res)=>{
+  const {value: doc} = await db.fetch({
+    "slug": req.params.slug
+}).next();
+  if(doc.length > 0) {
+    res.render(__dirname+"/views/notes.ejs", {notes: doc[0], url:'https://natandrustudy.com'+ req.originalUrl}) //replace with your domain
+  }else{
+    res.status(404).sendFile(__dirname + "/views/404.html");
+  }
+})
+
+app.post("/admin/makenotes", async (req,res)=>{
+  const options = {
+    method: 'POST',
+    url: 'https://api.short.io/links',
+    headers: {
+      Authorization: process.env.SHORT_API_KEY,
+    },
+    json: {
+      originalURL: 'https://natandrustudy.com/notes/'+req.body.slug, //replace with your domain
+      domain: 'natand.ru' //replace with your domain
+    },
+    responseType: 'json'
+  };
+  got(options).then(async (response) => {
+    const notes = await db.put({
+      title: req.body.title,
+      slug: req.body.slug,
+      description: req.body.description,
+      cdnurl: req.body.cdnurl,
+      cdnthumb: req.body.cdnthumb,
+      subject: req.body.subject,
+      shorturl: response.body.secureShortURL,
+      topic: "none",
+      numberintopic: 0
+  })
+  res.json(notes)
+  });
+})
+
 app.get("*", (req,res)=>{
-  res.sendFile(__dirname+"/views/404.html")
+  res.status(404).sendFile(__dirname+"/views/404.html")
 })
 
 const listener = app.listen(800, () => { //replace 800 with your port
